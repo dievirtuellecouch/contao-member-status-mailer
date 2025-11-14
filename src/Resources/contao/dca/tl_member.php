30c88aa79933e36304832ddbd8c4369f4212a6a6
websailing <info@websailing.de>
2025-11-15 00:27:39 +0100

    update

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -1,5 +1,12 @@
 <?php
 
+// Hide the regular Contao toggle (core eye) and prevent it from being added
+unset($GLOBALS['TL_DCA']['tl_member']['list']['operations']['toggle']);
+if (isset($GLOBALS['TL_DCA']['tl_member']['fields']['disable'])) {
+    $GLOBALS['TL_DCA']['tl_member']['fields']['disable']['toggle'] = false;
+    $GLOBALS['TL_DCA']['tl_member']['fields']['disable']['reverseToggle'] = false;
+}
+
 // Add list operation with custom toggle + mail
 $GLOBALS['TL_DCA']['tl_member']['list']['operations']['msm'] = [
     'label' => ['Aktivieren/Deaktivieren (mit Mail)', ''],
@@ -7,4 +14,3 @@ $GLOBALS['TL_DCA']['tl_member']['list']['operations']['msm'] = [
     'icon'  => 'visible.svg',
     'button_callback' => [\Websailing\MemberStatusMailerBundle\Service\StatusHandler::class, 'button']
 ];
-
